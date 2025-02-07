37cb56b7b71845c0ee433064e8c0fabffc0d17d6
Nishit Rana <nishitrana@Nishits-MacBook-Air.local>
2025-02-07 19:34:30 +0530

    Third

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   const [Exp, setExp] = useState("")
-  const [Result,setResult] = useState("0");
+  const [Result,setResult] = useState("");
 
   const handleClick = (value) => {
     setExp(prevExp => prevExp+value);
@@ -24,7 +24,7 @@ function App() {
     <button onClick={()=>{handleClick("7")}}>7</button> <button onClick={()=>{handleClick("8")}}>8</button> <button onClick={()=>{handleClick("9")}}>9</button> <button onClick={()=>{handleClick("+")}} >+</button><br/>
     <button onClick={()=>{handleClick("4")}}>4</button> <button onClick={()=>{handleClick("5")}}>5</button> <button onClick={()=>{handleClick("6")}}>6</button> <button onClick={()=>{handleClick("-")}}>-</button><br/>
     <button onClick={()=>{handleClick("1")}}>1</button> <button onClick={()=>{handleClick("2")}}>2</button> <button onClick={()=>{handleClick("3")}}>3</button> <button onClick={()=>{handleClick("*")}}>*</button><br/>
-    <button onClick={()=>{setExp(""); setResult("0");}}>C</button> <button onClick={()=>{handleClick("0")}}>0</button> <button onClick={() => {setResult(eval(Exp).toString())}}>=</button> <button onClick={()=>{handleClick("/")}}>/</button><br/>
+    <button onClick={()=>{setExp(""); setResult("");}}>C</button> <button onClick={()=>{handleClick("0")}}>0</button> <button onClick={() => {setResult(eval(Exp).toString())}}>=</button> <button onClick={()=>{handleClick("/")}}>/</button><br/>
     </div>                                                                                                                                        
    </div>
   )
