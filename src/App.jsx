a74ac18f1772818076b3aae814e72ceffb39b2f5
Nishit Rana <nishitrana@Nishits-MacBook-Air.local>
2025-02-07 19:44:07 +0530

    Fifth

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
    <div>
     <h1 style={{marginLeft:"450px"}}>React Calculator</h1>
-    <input value={Exp} readOnly id="input" style={{marginLeft:"500px"}}></input><br/>
+    <input type="text" value={Exp} readOnly id="input" style={{marginLeft:"500px"}}></input><br/>
     {
       Result && 
        <div style={{marginLeft:"550px", height:"20px"}}> {Result} </div>
@@ -24,7 +24,7 @@ function App() {
     <button onClick={()=>{handleClick("7")}}>7</button> <button onClick={()=>{handleClick("8")}}>8</button> <button onClick={()=>{handleClick("9")}}>9</button> <button onClick={()=>{handleClick("+")}} >+</button><br/>
     <button onClick={()=>{handleClick("4")}}>4</button> <button onClick={()=>{handleClick("5")}}>5</button> <button onClick={()=>{handleClick("6")}}>6</button> <button onClick={()=>{handleClick("-")}}>-</button><br/>
     <button onClick={()=>{handleClick("1")}}>1</button> <button onClick={()=>{handleClick("2")}}>2</button> <button onClick={()=>{handleClick("3")}}>3</button> <button onClick={()=>{handleClick("*")}}>*</button><br/>
-    <button onClick={()=>{setExp(""); setResult("");}}>C</button> <button onClick={()=>{handleClick("0")}}>0</button> <button onClick={() => {setResult(eval(Exp).toString())}}>=</button> <button onClick={()=>{handleClick("/")}}>/</button><br/>
+    <button onClick={()=>{setExp(""); setResult("");}}>C</button> <button onClick={()=>{handleClick("0")}}>0</button> <button onClick={() => {setResult(eval(Exp))}}>=</button> <button onClick={()=>{handleClick("/")}}>/</button><br/>
     </div>                                                                                                                                        
    </div>
   )
